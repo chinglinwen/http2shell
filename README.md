f0f5de00cb9d3a6bab077f8029fcc4f13033e57f
wenzhenglin <wenzhenglin@haodai.net>
2017-06-19 16:33:37 +0800

    Created.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # http2shell
 execute shell through http
+
+## Usage
+
+```
+Usage of ./http2shell:
+  -base string
+        base dir
+  -p string
+        listening port (default "9001")
+
+```
+
+## Note
+
+base flag used to restrict which command can be execute.
+
+## Example
+
+```
+curl -s localhost:9001/ -F path=echo -F args=aa
+```
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+	ospath "path"
+	"strings"
+
+	"upspin.io/log"
+)
+
+var (
+	port = flag.String("p", "9001", "listening port")
+	base = flag.String("base", "", "base dir")
+)
+
+func main() {
+	log.Printf("starting... base: %v", *base)
+	http.HandleFunc("/", cmdHandler)
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
+}
+
+func cmdHandler(w http.ResponseWriter, r *http.Request) {
+	path := r.FormValue("path")
+	if path == "" || strings.Contains(path, "..") {
+		fmt.Fprintf(w, "path is empty or include two dot\n")
+		return
+	}
+	if !strings.Contains(path, "./") {
+		if *base != "" {
+			path = ospath.Join(*base, path)
+		}
+	}
+	args := r.FormValue("args")
+
+	out, err := Run(path, []string{args})
+	if err != nil {
+		fmt.Fprintf(w, "%v\nerror: %v\n", out, err)
+	} else {
+		fmt.Fprintf(w, "%v", out)
+	}
+	var outline string
+	fmt.Sscanln(out, &outline)
+	log.Printf("cmd: %v, args: %v, out: %v, err: %v\n", path, args, outline, err)
+}
+
+func Run(path string, args []string) (string, error) {
+	cmd := exec.Command(path, args...)
+	out, err := cmd.CombinedOutput()
+	return string(out), err
+}
+
+func init() {
+	flag.Parse()
+	if *base == "./" {
+		*base = os.Getenv("PWD")
+	}
+}
