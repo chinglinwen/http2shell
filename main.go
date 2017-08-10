3197d05b69d7287ceaa81fe5565131bdfb92b012
wenzhenglin <wenzhenglin@haodai.net>
2017-08-10 13:53:46 +0800

    Add trace.

--- main.go
+++ main.go
@@ -11,19 +11,50 @@ import (
 
 	"github.com/chinglinwen/log"
 	"github.com/natefinch/lumberjack"
+
+	opentracing "github.com/opentracing/opentracing-go"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+	"github.com/uber/jaeger-client-go/rpcmetrics"
+
+	"github.com/uber/jaeger-lib/metrics/go-kit"
+	"github.com/uber/jaeger-lib/metrics/go-kit/expvar"
 )
 
 var (
 	port = flag.String("p", "9001", "listening port")
 	base = flag.String("base", "", "base dir")
+
+	metricsFactory = xkit.Wrap("", expvar.NewFactory(10))
 )
 
 func main() {
 	log.Printf("starting... base: %v", *base)
 	http.HandleFunc("/", cmdHandler)
+
+	//logger := log.NewFactory(logger.With(zap.String("service", "frontend")))
+
+	cfg := jaegercfg.Configuration{}
+	_, err := cfg.InitGlobalTracer("http2shell",
+		jaegercfg.Observer(rpcmetrics.NewObserver(metricsFactory.Namespace("http2shell", nil), rpcmetrics.DefaultNameNormalizer)),
+	)
+	if err != nil {
+		log.Fatalf("Could not initialize jaeger tracer: %s", err.Error())
+	}
+	//defer closer.Close()
+
 	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
 
+/*
+func init() {
+	cfg := jaegercfg.Configuration{}
+	closer, err := cfg.InitGlobalTracer("http2shell")
+	if err != nil {
+		log.Fatalf("Could not initialize jaeger tracer: %s", err.Error())
+	}
+	defer closer.Close()
+}
+*/
 func cmdHandler(w http.ResponseWriter, r *http.Request) {
 	path := r.FormValue("path")
 	if path == "" || strings.Contains(path, "..") {
@@ -36,6 +67,11 @@ func cmdHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	args := r.FormValue("args")
+
+	parent := opentracing.GlobalTracer().StartSpan("hello")
+	defer parent.Finish()
+	//sp := opentracing.StartSpan(path)
+	//defer sp.Finish()
 
 	out, err := Run(path, []string{args})
 	if err != nil {
