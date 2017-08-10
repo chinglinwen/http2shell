347c0383af415c033bde0d6b3d9d5dec1a1df044
wenzhenglin <wenzhenglin@haodai.net>
2017-08-10 16:28:11 +0800

    Make tracing works.

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"os/exec"
 	ospath "path"
 	"strings"
+	"time"
 	"unicode"
 
 	"github.com/chinglinwen/log"
@@ -15,47 +18,46 @@ import (
 
 	opentracing "github.com/opentracing/opentracing-go"
 	jaegercfg "github.com/uber/jaeger-client-go/config"
-	"github.com/uber/jaeger-client-go/rpcmetrics"
-
-	"github.com/uber/jaeger-lib/metrics/go-kit"
-	"github.com/uber/jaeger-lib/metrics/go-kit/expvar"
 )
 
 var (
-	port = flag.String("p", "9001", "listening port")
-	base = flag.String("base", "", "base dir")
-
-	metricsFactory = xkit.Wrap("", expvar.NewFactory(10))
+	port      = flag.String("p", "9001", "listening port")
+	base      = flag.String("base", "", "base dir")
+	dotracing = flag.Bool("t", false, "enable jaeger tracing")
 )
 
 func main() {
 	log.Printf("starting... base: %v", *base)
-	http.HandleFunc("/", cmdHandler)
-
-	//logger := log.NewFactory(logger.With(zap.String("service", "frontend")))
-
-	cfg := jaegercfg.Configuration{}
-	_, err := cfg.InitGlobalTracer("http2shell",
-		jaegercfg.Observer(rpcmetrics.NewObserver(metricsFactory.Namespace("http2shell", nil), rpcmetrics.DefaultNameNormalizer)),
-	)
-	if err != nil {
-		log.Fatalf("Could not initialize jaeger tracer: %s", err.Error())
+	closer := InitTracer()
+	if closer != nil {
+		defer closer.Close()
 	}
-	//defer closer.Close()
-
+	http.HandleFunc("/", cmdHandler)
 	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
 
-/*
-func init() {
-	cfg := jaegercfg.Configuration{}
-	closer, err := cfg.InitGlobalTracer("http2shell")
-	if err != nil {
-		log.Fatalf("Could not initialize jaeger tracer: %s", err.Error())
+func InitTracer() io.Closer {
+	if *dotracing {
+		cfg := jaegercfg.Configuration{
+			Sampler: &jaegercfg.SamplerConfig{
+				Type:  "const",
+				Param: 1,
+			},
+			Reporter: &jaegercfg.ReporterConfig{
+				LogSpans:            true,
+				BufferFlushInterval: 1 * time.Second,
+			},
+		}
+		tracer, closer, err := cfg.New("http2shell")
+		if err != nil {
+			log.Fatalf("Could not initialize jaeger tracer: %s", err.Error())
+		}
+		opentracing.SetGlobalTracer(tracer)
+		return closer
 	}
-	defer closer.Close()
+	return nil
 }
-*/
+
 func cmdHandler(w http.ResponseWriter, r *http.Request) {
 	s := r.FormValue("run")
 
@@ -84,12 +86,11 @@ func cmdHandler(w http.ResponseWriter, r *http.Request) {
 		args[i] = strings.Trim(v, `'"`)
 	}
 
-	parent := opentracing.GlobalTracer().StartSpan("hello")
+	parent := opentracing.GlobalTracer().StartSpan("http")
 	defer parent.Finish()
-	//sp := opentracing.StartSpan(path)
-	//defer sp.Finish()
 
-	out, err := Run(path, args)
+	ctx := opentracing.ContextWithSpan(context.TODO(), parent)
+	out, err := Run(ctx, path, args)
 	if err != nil {
 		fmt.Fprintf(w, "%v\nerror: %v\n", out, err)
 	} else {
@@ -119,7 +120,10 @@ func fieldsFunc() func(c rune) bool {
 	}
 }
 
-func Run(path string, args []string) (string, error) {
+func Run(ctx context.Context, path string, args []string) (string, error) {
+	sp, _ := opentracing.StartSpanFromContext(ctx, "cmd")
+	defer sp.Finish()
+
 	cmd := exec.Command(path, args...)
 	out, err := cmd.CombinedOutput()
 	return string(out), err
